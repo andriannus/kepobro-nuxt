086ccda7b196fb39367696f9abb95ced230e1322
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2019-07-27 22:07:24 +0700

    create coponent for show news content

--- pages/business/index.vue
+++ pages/business/index.vue
@@ -1,89 +1,21 @@
 <template>
-  <div class="card">
-    <header class="card-header">
-      <span class="card-header-title">
-        Business
-      </span>
-
-      <a
-        v-if="isLoading === false"
-        class="card-header-icon"
-        title="Refresh News"
-        @click="getNews()"
-      >
-        <span class="icon">
-          <i class="fas fa-sync-alt" />
-        </span>
-      </a>
-    </header>
-
-    <div class="card-content">
-      <progress
-        v-if="isLoading === true"
-        class="progress is-small is-dark"
-        max="100"
-      >
-        15%
-      </progress>
-
-      <div v-if="isLoading === false">
-        <article
-          v-for="(article, index) in articles"
-          :key="index"
-          class="media"
-        >
-          <figure class="media-left mt-5">
-            <p class="image is-64x64">
-              <img
-                v-if="!!article.urlToImage === true"
-                :src="article.urlToImage"
-                onerror="this.src='https://bulma.io/images/placeholders/480x320.png'"
-              >
-
-              <img
-                v-if="!!article.urlToImage === false"
-                src="https://bulma.io/images/placeholders/480x320.png"
-              >
-            </p>
-          </figure>
-
-          <div class="media-content">
-            <div
-              class="content"
-            >
-              <p class="has-text-justified mb-10">
-                <a @click="readArticle(article)">
-                  <strong>{{ article.title }}</strong>
-                </a>
-              </p>
-              <span>
-                <i class="fas fa-globe mr-5" />
-
-                {{ article.source.name }}
-              </span>
-            </div>
-          </div>
-        </article>
-      </div>
-    </div>
-
-    <footer
-      v-if="isLoading === false"
-      class="card-footer"
-    >
-      <div class="card-footer-item">
-        <span>
-          <em>All displayed</em>
-        </span>
-      </div>
-    </footer>
-  </div>
+  <TheNewsContent
+    :articles="articles"
+    :is-loading="isLoading"
+    :title="title"
+  />
 </template>
 
 <script>
 import { API } from '~/assets/consts/api'
 
+const TheNewsContent = () => import('~/components/TheNewsContent')
+
 export default {
+  components: {
+    TheNewsContent
+  },
+
   head() {
     return {
       title: 'Business - KepoBro News',
@@ -130,7 +62,8 @@ export default {
   data() {
     return {
       articles: [],
-      isLoading: false
+      isLoading: true,
+      title: 'Business'
     }
   },
 
@@ -152,12 +85,6 @@ export default {
         .finally(() => {
           this.isLoading = false
         })
-    },
-
-    readArticle(article) {
-      localStorage.setItem('article', JSON.stringify(article))
-
-      this.$router.push('/read')
     }
   }
 }
